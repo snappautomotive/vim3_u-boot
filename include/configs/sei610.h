d3f6849a8723ec7a48b597d0c10d1d767344f052
Guillaume La Roque <glaroque@baylibre.com>
2019-10-18 13:38:50 +0200

    configs: sei610: add support of android A/B partitions
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- include/configs/sei610.h
+++ include/configs/sei610.h
@@ -16,15 +16,32 @@
 #define USERDATA_UUID "9b976e42-5207-11e9-8f16-ff47ac594b22;"
 #define ROOT_UUID "ddb8c3f6-d94d-4394-b633-3134139cc2e0;"
 
+#if defined(CONFIG_CMD_AB_SELECT)
+#define PARTS_DEFAULT                                        \
+	"uuid_disk=${uuid_gpt_disk};"  	\
+	"name=logo,start=512K,size=2M,uuid=" LOGO_UUID             \
+	"name=misc,size=512K,uuid=${uuid_gpt_misc};" \
+	"name=vbmeta_a,size=512K,uuid=${uuid_gpt_vbmeta_a};" \
+	"name=vbmeta_b,size=512K,uuid=${uuid_gpt_vbmeta_b};" \
+	"name=boot_a,size=32M,bootable,uuid=${uuid_gpt_boot_a};" \
+	"name=boot_b,size=32M,bootable,uuid=${uuid_gpt_boot_b};" \
+	"name=super,size=3072M,uuid=${uuid_gpt_super};"	\
+	"name=userdata,size=11298M,uuid=${uuid_gpt_userdata};" \
+	"name=rootfs,size=-,uuid=" ROOT_UUID
+
+#else
 #define PARTS_DEFAULT                                        \
 	"uuid_disk=${uuid_gpt_disk};"  			\
-	"name=boot,size=64M,bootable,uuid=${uuid_gpt_boot};" \
-	"name=logo,size=2M,uuid=" LOGO_UUID             \
-	"name=cache,size=256M,uuid=" CACHE_UUID             \
-	"name=system,size=1536M,uuid=" SYSTEM_UUID           \
-	"name=vendor,size=256M,uuid=" VENDOR_UUID            \
-	"name=userdata,size=12795M,uuid=" USERDATA_UUID	\
+	"name=logo,start=512K,size=2M,uuid=" LOGO_UUID             \
+	"name=misc,size=512K,uuid=${uuid_gpt_misc};" \
+	"name=vbmeta,size=512K,uuid=${uuid_gpt_vbmeta};" \
+	"name=boot,size=32M,bootable,uuid=${uuid_gpt_boot};" \
+	"name=recovery,size=32M,uuid=${uuid_gpt_recovery};"	\
+	"name=cache,size=256M,uuid=${uuid_gpt_cache};"	\
+	"name=super,size=1792M,uuid=${uuid_gpt_super};"	\
+	"name=userdata,size=12794M,uuid=${uuid_gpt_userdata};" \
 	"name=rootfs,size=-,uuid=" ROOT_UUID
+#endif
 
 #include <configs/meson64_android.h>
 
